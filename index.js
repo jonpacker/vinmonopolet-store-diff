785cde41fdbb1f8c4a83bd7f10cefcee1d3be744
Jon Packer <contact@jonpacker.com>
2018-03-19 21:42:20 +0100

    add privateApp for private methods

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const moment = require('moment');
 moment.locale('nb');
 const dev = process.env.NODE_ENV == 'development';
 
-var app = new Koa();
+const app = new Koa();
+const privateApp = new Koa();
+
 const router = app.router = new Router();
+const privateRouter = privateApp.router = new Router();
+
 app.redis = redis;
 const pug = new Pug({ 
   viewPath: './views',
@@ -21,7 +25,7 @@ const pug = new Pug({
 
 app.storeDir = require('minimist')(process.argv.slice(2))['store-dir'];
 
-require('./routes/diff')(app);
+require('./routes/diff')(app, privateApp);
 
 if (dev) app.use(require('koa-logger')());
 app.use(router.routes());
@@ -30,4 +34,9 @@ app.use(require('./lib/stylus-middleware')('./public'));
 app.use(require('koa-static')('./public'));
 pug.use(app);
 
+if (dev) privateApp.use(require('koa-logger')());
+privateApp.use(privateRouter.routes());
+privateApp.use(privateRouter.allowedMethods);
+
 app.listen(10005);
+privateApp.listen(10004);
