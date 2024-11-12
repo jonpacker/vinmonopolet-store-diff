29fe0be164f0d8f5f879f64590cebcc49b3bd132
Jon Packer <contact@jonpacker.com>
2024-11-12 12:43:21 +0100

    in-place reloading of data on stock page

--- public/js/vintappd-refresh.js
+++ public/js/vintappd-refresh.js
@@ -1,12 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
+  /**
+   *
+              a.utrating(href=`https://untappd.com/b/_/${product.bid}`)
+                if product.rating
+                  | #{product.rating}
+                  */
   const buttons = document.querySelectorAll('.refresh-rating')
   for (const button of buttons) {
     button.addEventListener('click', async function (event) {
       event.preventDefault()
       const vpcode = button.dataset.vpcode
       button.classList.add('loading')
-      await fetch(`https://vintappd.jonpacker.com/vp/${vpcode}/refresh`, { method: 'POST' })
-      document.location = document.location + '?clean=1'
+      const response = await fetch(`https://vintappd.jonpacker.com/vp/${vpcode}/refresh`, { method: 'POST' })
+      const updated = await response.json()
+      const oldrating = document.querySelector(`.utrating[data-vpcode="${vpcode}"]`)
+      oldrating.innerHTML = updated.beer.rating_score || '-'
+      button.classList.remove('loading')
+      //document.location = document.location + '?clean=1'
     })
   }
   const linkButtons = document.querySelectorAll('.autolink')
@@ -17,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
       const promptElement = button.previousElementSibling
       const prompt = promptElement.value
       button.classList.add('loading')
-      await fetch(`https://vintappd.jonpacker.com/vp/autolink/${vpcode}`, {
+      const response = await fetch(`https://vintappd.jonpacker.com/vp/autolink/${vpcode}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -26,7 +36,14 @@ document.addEventListener('DOMContentLoaded', function () {
           prompt
         } : {})
       })
-      document.location = document.location + '?clean=1'
+      const link = await response.json()
+      const column = document.querySelector(`.vintappd-col[data-vpcode="${vpcode}"]`)
+      column.innerHTML = `
+        <a class='utrating' href='https://untappd.com/b/_/${link.beer.bid}'>
+          ${link.beer.rating_score}
+          ✅
+        </a>
+      `
     })
   }
 
